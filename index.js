a7307b927be8008f048b799999420dddca4dd3e4
Jan Rodson Burgos <janrburgos@gmail.com>
2021-05-19 17:40:13 +0800

    feat: added project details when clicked on a project

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
+// nav-list and hamburger menu
+
 const hamburger = document.querySelector(".hamburger");
 const siteNav = document.querySelector(".site-nav");
+
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("open");
   siteNav.classList.toggle("open");
+});
+
+// projects details overlay
+
+const overlay = document.querySelector(".details-overlay");
+const projects = document.querySelectorAll(".project");
+const details = document.querySelectorAll(".details-container");
+
+projects.forEach((project, i) => {
+  project.addEventListener("click", () => {
+    overlay.classList.add("open");
+    details[i].classList.add("open");
+  });
+});
+
+overlay.addEventListener("click", () => {
+  overlay.classList.remove("open");
+  details.forEach((detail) => {
+    detail.classList.remove("open");
+  });
 });
